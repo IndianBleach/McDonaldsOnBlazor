7a9069dfebed02ecfdc1255ede145ec60926b61f
IndianBleach <84047808+IndianBleach@users.noreply.github.com>
2022-07-03 18:15:14 +0300

    Add: user can append items to basket with options

--- src/McDonalds.Common/DTOs/BasketDTOs/BasketItemDto.cs
+++ src/McDonalds.Common/DTOs/BasketDTOs/BasketItemDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using McDonaldsOnWeb.Common.Entities.ItemEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,19 +7,15 @@ using System.Threading.Tasks;
 
 namespace McDonaldsOnWeb.Common.DTOs.BasketDTOs
 {
-    public class SelectedMenuItemDto
-    {
-        public int Id { get; set; }
-    }
-
     public class SelectedItemOptionIngredientDto
     {
         public int Id { get; set; }
+        public bool IsAppendToMenuItem { get; set; }
     }
 
     public class BasketItemDto
     {
-        public SelectedMenuItemDto MenuItem { get; set; }
+        public MenuItemDto MenuItem { get; set; }
         public ICollection<SelectedItemOptionIngredientDto> OptionIngredients { get; set; }
     }
 }
